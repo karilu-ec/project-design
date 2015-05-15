97971cefbad223a6bb87917a0f396e57ee3fd1d4
Karina Simpson <karilu_ec@yahoo.com>
2015-05-15 07:24:32 -0400

    basis bar

---
+++ js/d3BarBasic.js
@@ -0,0 +1,11 @@
+//http://www.recursion.org/d3-for-mere-mortals/
+var rect_demo = d3.select("#rect-demo")
+        .append("svg")
+        .attr("width", 400)
+        .attr("height", 300);
+    
+    rect-demo.append("rect")
+        .attr("x", 100)
+        .attr("y", 100)
+        .attr("width", 200)
+        .attr("height", 100);
