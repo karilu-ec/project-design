3ad36cf7124b48b76edf373848e95c8883319982
Karina Simpson <ksimpson@usna.edu>
2015-05-20 15:55:38 -0400

    transition totals in portrait chart 3 first data sets

--- js/portrait.js
+++ js/portrait.js
@@ -10,6 +10,8 @@ var svg = d3.select("#chart").append("svg")
   
 // Get the right Axis for the chart.
 function putAxis(titleY, x, y) {
+  //Remove previus axis
+  d3.select("#chart").selectAll("g.axis").remove();
     //xAxis Scale
   var xAxis = d3.svg.axis()
 	  .scale(x)
@@ -22,19 +24,19 @@ function putAxis(titleY, x, y) {
 	  .ticks(20);
 
    //Adding the xAxis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
+  var xAxisGroup = svg.append("g")
+	  .attr("class", "x axis")
+	  .attr("transform", "translate(0," + height + ")")
+	  .call(xAxis)
     .selectAll("text")
       .attr("text-anchor", "end")
       .attr("dx", ".5em")
       .attr("dy", ".80em");
       
   //Adding the yAxis
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
+  var yAxisGroup = svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
   .append("text")
     .attr("transform", "rotate(-90)")
     .attr("text-anchor", "end")
@@ -44,7 +46,10 @@ function putAxis(titleY, x, y) {
 }
 
 //Bars maker.
-function maleFemalebars(data) {
+function maleFemalebars(data, dataTitle) {
+  //Remove previous Bar Text
+  d3.select("#chart").selectAll("text.textBar").remove();
+  
   // X and Y scale range
   var x = d3.scale.ordinal().rangeRoundBands([0, width], .5);
   var y = d3.scale.linear().range([height, 0]);
@@ -57,50 +62,75 @@ function maleFemalebars(data) {
   var color = d3.scale.category10();
   
   //Set axis
-  putAxis("Number of Applications", x, y);
-
+  putAxis(dataTitle, x, y);
+  
+  //Calculate totals
+  var total = d3.nest()
+	//.key(function(d) { return d.keyColumn})  //For this particular example I don't have a key column.
+	.rollup(function(d) {
+	  return d3.sum(d, function(g) { return g.value; })
+	}).entries(data);
   
   //Bind with data
  var barsData= svg.selectAll("rect.bar")
     .data(data);
  var bars = barsData.enter();
-   
+ var barsText = svg.selectAll("text.textBar")
+    .data(data);
+ 
    //update
    barsData
-   .attr("fill", function(d,i) { return color(i); } );
+   .attr("fill", function(d,i) { return color(i); } )
+
     
   //enter()
   barsData.enter()
       .append("rect")
       .attr("class", "bar")    
       .attr("fill", function(d,i) { return color(i); } );
-      //.attr("x", function(d) { return x(d.gender); })
-      //.attr("y", function(d) { return y(d.value); })
-      //.attr("width", x.rangeBand())
-      //.attr("height", function(d) { return height - y(d.value); });
-  
+	  
   //exit
- // bars.exit()
-    /* bars
+  barsData.exit()
 	.transition()
 	.duration(300)
 	.ease("exp")
-	  .attr("width", 0)
-	  .remove()*/
+	.attr("width", 0)
+	.remove()
 	  
-  barsData.attr("stroke-width", 4)
+  barsData
+     .on("mouseover", function(d) {
+	  //get this bar x/y values and augment for the tooltip
+	  var xPosition = parseFloat(d3.select(this).attr("x")) + x.rangeBand()/2;
+	  var yPosition = parseFloat(d3.select(this).attr("y"))/2 + height/2;
+	  
+	  //update the tooltip position
+	  var tooltip = d3.select("#tooltip")
+		.style("left", xPosition + "px")
+		.style("top", yPosition + "px");
+	  tooltip.select("#title")
+		  .text(dataTitle)
+	  tooltip.select("#value")
+		  .text("Total: " + total);
+	  
+	  //show the tooltip
+	  d3.select("#tooltip").classed("hide", false);	  
+	})
+	.on("mouseout", function(d) {
+	  //hide the tooltip
+	  d3.select("#tooltip").classed("hide", true);
+	})
+	.attr("stroke-width", 4)
 	.transition()
-	.duration(300)
+	.duration(800)
 	.ease("quad")
 	.attr("x", function(d) { return x(d.gender); })
-        .attr("y", function(d) { return y(d.value); })
-        .attr("width", x.rangeBand())
-        .attr("height", function(d) { return height - y(d.value); });
- 
+    .attr("y", function(d) { return y(d.value); })
+    .attr("width", x.rangeBand())
+    .attr("height", function(d) { return height - y(d.value); });
 
-  bars
-      .append("text")
-      .attr("x", function(d) { return x(d.gender); })
+  barsText
+	  .enter().append("text") //operate in new elements only
+	  .attr("x", function(d) { return x(d.gender); })
       .attr("y", function(d) { return y(d.value); })
       .attr("dx", x.rangeBand()/2)
       .attr("dy", "1.2em")
@@ -115,8 +145,9 @@ function maleFemalebars(data) {
 queue()
 	.defer(d3.csv, "application.csv")
 	.defer(d3.csv, "offersAppointment.csv")
+	.defer(d3.csv, "classSize.csv")
 	.await(init);
-function init(error, applications, offersAppointments) {
+function init(error, applications, offersAppointments, classSize) {
   if (error) {     console.log(error);  }
   applications.forEach(function(d) {   
     d.value= +d.value;
@@ -124,12 +155,21 @@ function init(error, applications, offersAppointments) {
   offersAppointments.forEach(function(d) {   
     d.value= +d.value;
   });
-  /*console.log(applications);
-  console.log(offersAppointments);*/
   
   //Default to first chart
-  maleFemalebars(applications)
-  
-  
+  maleFemalebars(applications, "Number of Applications")
+    
   //Event listeners
+  d3.select("#applications")
+	.on("click", function(d,i) {
+	  maleFemalebars(applications, "Number of Applications")
+	});
+  d3.select("#offersAppointment")
+	.on("click", function(d,i) {
+	  maleFemalebars(offersAppointments, "Offers of Appointment")
+	});
+  d3.select("#classSize")
+	.on("click", function(d,i) {
+	  maleFemalebars(classSize, "Class Size")
+	});
 }
