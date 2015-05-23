874951eb9c45f2d357bc6df50aab5540d1721ad7
Karina Simpson <karilu_ec@yahoo.com>
2015-05-23 01:53:04 -0400

    Added Race breakdown. Fixed the update, exit , enter states in bars.

--- js/portrait.js
+++ js/portrait.js
@@ -1,4 +1,4 @@
-var margins= {top:50, right:40, bottom:30, left:100};
+var margins= {top:50, right:40, bottom:30, left:50};
 var width = 1000 - margins.left - margins.right;
 var height = 500 - margins.top - margins.bottom;
 var chartNode = d3.select("#chart").node();
@@ -7,8 +7,20 @@ var svg = d3.select("#chart").append("svg")
     .attr("width", width + margins.left + margins.right)
     .attr("height", height + margins.top + margins.bottom)
   .append("g")
-	.attr("class", "mainG")
+    .attr("class", "mainG")
     .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
+var svgTitle = d3.select("svg").append("g")
+    .attr("class", "chartTitle")
+    .attr("transform", "translate("+ (width-4*margins.right)+","+ margins.top + ")");
+
+// Set the p for the Charts Legends
+  var tooltip	= d3.select("#tooltip");
+  tooltip.append("p")
+    .attr("class", "category");
+  tooltip.append("p")
+    .attr("class", "total");
+  tooltip.append("p")
+    .attr("class", "percent");		
   
 // Get the right Axis for the chart.
 function putAxis(titleY, x, y) {
@@ -49,10 +61,12 @@ function putAxis(titleY, x, y) {
 
 //Male and Female Bars maker.
 function maleFemalebars(data, dataTitle, x, y) {
-  //Remove previous Bar Text
+  //Remove previous charts elements
   d3.select("#chart").selectAll("text.textBar").remove();
-  //Remove previous Pie charts
   d3.select("#chart").selectAll("path").remove();
+  d3.select("#chart").selectAll("g.donutLegend").remove();
+  d3.select("#chart").selectAll("g.donutLegendTitle").remove();
+  svgTitle.selectAll("text").remove();
   var mainG = d3.select(".mainG")
     .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
   
@@ -68,7 +82,13 @@ function maleFemalebars(data, dataTitle, x, y) {
 	.rollup(function(d) {
 	  return d3.sum(d, function(g) { return g.value; })
 	}).entries(data);
-  
+ 
+  //Set chart title
+  svgTitle.append("text").text(dataTitle);
+  svgTitle.append("text")
+    .attr("y", 20)
+    .text("Total: " + total);
+    
   //Bind with data
  var barsData= svg.selectAll("rect.bar")
     .data(data);
@@ -78,14 +98,24 @@ function maleFemalebars(data, dataTitle, x, y) {
  
    //update
    barsData
-   .attr("fill", function(d,i) { return color(i); } )
+    .transition()	
+    .duration(800)
+    .ease("quad")
+    .attr("x", function(d) { return x(d.category); })
+    .attr("y", function(d) { return y(d.value); })
+    .attr("width", x.rangeBand())
+    .attr("height", function(d) { return height - y(d.value); })
+    .attr("fill", function(d,i) { return color(i); } );
     
   //enter()
   barsData.enter()
       .append("rect")
       .attr("class", "bar")    
-      .attr("fill", function(d,i) { return color(i); } );
-	  
+      .attr("fill", function(d,i) { return color(i); } )
+      .attr("x", function(d) { return x(d.category); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", x.rangeBand())
+      .attr("height", function(d) { return height - y(d.value); });	  
   //exit
   barsData.exit()
 	.transition()
@@ -93,6 +123,7 @@ function maleFemalebars(data, dataTitle, x, y) {
 	.ease("exp")
 	.attr("width", 0)
 	.remove()
+  
 	  
   barsData
      .on("mouseover", function(d) {
@@ -102,13 +133,11 @@ function maleFemalebars(data, dataTitle, x, y) {
 	  
 	  //update the tooltip position
 	  var tooltip = d3.select("#tooltip")
-		.style("left", xPosition + "px")
-		.style("top", yPosition + "px");
+	    .style("left", xPosition + "px")
+	    .style("top", yPosition + "px");
 	  tooltip.select("#title")
-		  .text(dataTitle)
-	  tooltip.append("div")
-		  .attr("class", "total");		  
-	  tooltip.select(".total").html("<strong>Total: </strong>" + total)
+	    .text(dataTitle)		  
+	  tooltip.select(".total").text(d.category +": "+d.value);
 	  
 	  //show the tooltip
 	  d3.select("#tooltip").classed("hide", false);	  
@@ -116,19 +145,11 @@ function maleFemalebars(data, dataTitle, x, y) {
 	.on("mouseout", function(d) {
 	  //hide the tooltip
 	  d3.select("#tooltip").classed("hide", true);
-	})
-	.attr("stroke-width", 4)
-	.transition()	
-	.duration(800)
-	.ease("quad")
-	.attr("x", function(d) { return x(d.gender); })
-    .attr("y", function(d) { return y(d.value); })
-    .attr("width", x.rangeBand())
-    .attr("height", function(d) { return height - y(d.value); });
+	});
 
   barsText
 	  .enter().append("text") //operate in new elements only
-	  .attr("x", function(d) { return x(d.gender); })
+	  .attr("x", function(d) { return x(d.category); })
       .attr("y", function(d) { return y(d.value); })
       .attr("dx", x.rangeBand()/2)
       .attr("dy", "-.5em")
@@ -147,6 +168,7 @@ function nominationsPie(data) {
   d3.select("#chart").selectAll("text.textBar").remove();
   d3.select("#chart").selectAll("g.axis").remove();
   d3.select("#chart").selectAll("rect.bar").remove();
+  svgTitle.selectAll("text").remove();
   
   var outerRadius = Math.min(width, (height+margins.top))/2;
   var donutWidth = 65;
@@ -154,6 +176,9 @@ function nominationsPie(data) {
   //http://bl.ocks.org/mbostock/5577023 <- color brewer pallete
   var color = d3.scale.ordinal()
 	.range(["#66c2a5","#8da0cb","#e78ac3","#a6d854","#ffd92f","#e5c494","#b3b3b3"]);
+  
+  //Define color domain
+  d3.map(data, function(d) { return color(d.category);});
   
   //Centering the group
   d3.select(".mainG")	  
@@ -173,23 +198,45 @@ function nominationsPie(data) {
   var tooltip	= d3.select("#tooltip");
 	tooltip.select("#title")
 		.text("Nominating Categories");
-	tooltip.append("div")
-		.attr("class", "category");
-	tooltip.append("div")
-		.attr("class", "count");
-	tooltip.append("div")
-		.attr("class", "percent");
 
   //Set the arc and draw the paths for the donut
-	var	path = svg.selectAll("path")
+	var path = svg.selectAll("path")
 		.data(pie(data)) //data is receiving the pie function with the actual data.
 		.enter()
 		.append("path")
 		.attr("d", arc)
 		.attr("fill", "#ddd")
 		.each(function(d) { this._current = d; }); //store the initial angles.
-	
-    d3.map(data, function(d) { return color(d.category);});	
+	//Event handler for the donut sections
+	path.on("mouseover", function(d) {
+		var total = d3.sum(data.map(function(d) {
+			return d.value;
+		}));
+		var percent = Math.round((100*d.data.value)/total);
+		
+		//Calculate the absolute coordinates
+		var absoluteMousePos = d3.mouse(chartNode);
+		
+		var xPos = absoluteMousePos[0];
+		var yPos = absoluteMousePos[1]+150;
+		
+		tooltip.select(".category").html(d.data.category.toUpperCase());
+		tooltip.select(".total").html("<strong>Total: </strong>"  + d.data.value);
+		tooltip.select(".percent").html(percent + "%");
+		//show the tooltip
+		tooltip.classed("hide", false);
+		//update the tooltip's position
+		tooltip.style("left", xPos + "px");
+		tooltip.style("top", yPos + "px");
+	});
+	path.on("mouseout", function(d) {
+		//hide the tooltip
+		tooltip.classed("hide", true);
+		tooltip.select(".category").html("");
+		tooltip.select(".total").html("");
+		tooltip.select(".percent").html("");
+	});
+    
 	var timeout = setTimeout(function() {
 	  clearTimeout(timeout);
 	  fillPie()
@@ -271,8 +318,9 @@ queue()
 	.defer(d3.csv, "offersAppointment.csv")
 	.defer(d3.csv, "classSize.csv")
 	.defer(d3.csv, "nominatingCategory.csv")
+	.defer(d3.csv, "raceBreakdown.csv")
 	.await(init);
-function init(error, applications, offersAppointments, classSize, nominatingCategory) {
+function init(error, applications, offersAppointments, classSize, nominatingCategory, raceBreakdown) {
   if (error) {     console.log(error);  }
   applications.forEach(function(d) {   
     d.value= +d.value;
@@ -283,7 +331,10 @@ function init(error, applications, offersAppointments, classSize, nominatingCate
   classSize.forEach(function(d) {   
     d.value= +d.value;
   });
-  nominatingCategory.forEach(function(d) {   
+  nominatingCategory.forEach(function(d) { 
+    d.value= +d.value;
+  });
+  raceBreakdown.forEach(function(d) {   
     d.value= +d.value;
   });
   
@@ -293,7 +344,7 @@ function init(error, applications, offersAppointments, classSize, nominatingCate
   var y = d3.scale.linear().range([height, 0]);
   
   // X and Y scale  domain of the number of applications.
-  x.domain(applications.map(function(d) { return d.gender; })); // x domain in male and female
+  x.domain(applications.map(function(d) { return d.category; })); // x domain in male and female applicants
   y.domain([0, d3.max(applications, function(d) { return d.value; }) ]);
   
   maleFemalebars(applications, "Number of Applications", x, y)
@@ -301,18 +352,34 @@ function init(error, applications, offersAppointments, classSize, nominatingCate
   //Event listeners
   d3.select("#applications")
 	.on("click", function(d,i) {
+	  // X and Y scale  domain of the number of applications.
+	  x.domain(applications.map(function(d) { return d.category; })); // x domain in male and female applicants
+	  y.domain([0, d3.max(applications, function(d) { return d.value; }) ]);
 	  maleFemalebars(applications, "Number of Applications", x, y)
 	});
   d3.select("#offersAppointment")
 	.on("click", function(d,i) {
+	  // X and Y scale  domain of the number of applications and offers of appointment.
+	  x.domain(offersAppointments.map(function(d) { return d.category; })); // x domain in male and female
+	  y.domain([0, d3.max(applications, function(d) { return d.value; }) ]);
 	  maleFemalebars(offersAppointments, "Offers of Appointment", x, y)
-	});
-  d3.select("#classSize")
-	.on("click", function(d,i) {
-	  maleFemalebars(classSize, "Class Size", x, y)
 	});
   d3.select("#nominatingCategory")
 	.on("click", function(d,i) {
 	  nominationsPie(nominatingCategory)
+	});
+  d3.select("#classSize")
+	.on("click", function(d,i) {
+	   // X and Y scale  domain of the class size.
+	  x.domain(classSize.map(function(d) { return d.category; }));
+	  y.domain([0, d3.max(classSize, function(d) { return d.value; }) ]);
+	  maleFemalebars(classSize, "Class Size", x, y)
+	});
+  d3.select("#racialBreakdown")
+	.on("click", function(d,i) {
+	   // X and Y scale  domain of the race breakdown.
+	  x.domain(raceBreakdown.map(function(d) { return d.category; }));
+	  y.domain([0, d3.max(classSize, function(d) { return d.value; }) ]);
+	  maleFemalebars(raceBreakdown, "Racial Breakdown", x, y)
 	});
 }
